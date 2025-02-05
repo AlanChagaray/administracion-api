47f6daa29620fc3146863f0321d927bf794d7e28
AlanChagaray <alanandreschagaray@gmail.com>
2025-02-05 15:10:40 -0300

    Build_fix

--- src/app/api/usuarios/controller.ts
+++ src/app/api/usuarios/controller.ts
@@ -1,11 +1,7 @@
 import { NextResponse , NextRequest} from "next/server";
 import Usuario from './model';
 
-<<<<<<< HEAD
 export async function buscar(request : NextRequest) {
-=======
-export async function buscar(request : any) {
->>>>>>> 86b409e395205518c540018ab8e6a9c5da69a607
   const { searchParams } = new URL(request.url);
   const usuario = searchParams.get('usuario') || null;
   const password = searchParams.get('password') || null;
